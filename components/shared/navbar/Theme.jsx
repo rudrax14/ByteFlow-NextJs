56f2f65863df293a0412aa74499f5a8e74ff67e2
rudrax14 <m.rudra2003@gmail.com>
2024-06-08 21:54:36 +0530

    refactor: Update mobile navigation styles and add LeftSidebar component

--- components/shared/navbar/Theme.jsx
+++ components/shared/navbar/Theme.jsx
@@ -1,26 +1,23 @@
 "use client";
 
 import React from "react";
-
 import { useTheme } from "@/context/ThemeProvider";
-
 import {
   Menubar,
   MenubarContent,
-  MenubarItem,
   MenubarMenu,
   MenubarTrigger,
+  MenubarItem,
 } from "@/components/ui/menubar";
 import Image from "next/image";
 import { themes } from "@/constants";
 
 const Theme = () => {
   const { mode, setMode } = useTheme();
-
   return (
     <Menubar className="relative border-none bg-transparent shadow-none">
       <MenubarMenu>
-        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+        <MenubarTrigger className="cursor-pointer focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
           {mode === "light" ? (
             <Image
               src="/assets/icons/sun.svg"
@@ -39,36 +36,37 @@ const Theme = () => {
             />
           )}
         </MenubarTrigger>
-        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
-          {themes.map((item) => (
+        <MenubarContent className="absolute -right-12 mt-3 min-w-[120px] rounded border bg-white py-2 dark:border-dark-400 dark:bg-dark-300">
+          {themes.map((theme) => (
             <MenubarItem
-              key={item.value}
-              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              key={theme.value}
               onClick={() => {
-                setMode(item.value);
+                // set to light or dark mode and add that value to localStorage else remove value from localStorage to set theme to user's operating system
 
-                if (item.value !== "system") {
-                  localStorage.theme = item.value;
+                setMode(theme.value);
+                if (theme.value !== "system") {
+                  localStorage.theme = theme.value;
                 } else {
                   localStorage.removeItem("theme");
                 }
               }}
+              className="flex  cursor-pointer items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
             >
               <Image
-                src={item.icon}
-                alt={item.value}
+                src={theme.icon}
+                alt={theme.value}
                 width={16}
                 height={16}
-                className={`${mode === item.value && "active-theme"}`}
+                className={`${mode === theme.value && "active-theme"}`}
               />
               <p
                 className={`body-semibold text-light-500 ${
-                  mode === item.value
+                  mode === theme.value
                     ? "text-primary-500"
                     : "text-dark100_light900"
                 }`}
               >
-                {item.label}
+                {theme.label}
               </p>
             </MenubarItem>
           ))}
