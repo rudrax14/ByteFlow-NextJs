bc7d1455045f699024a965aefb67f64ac2320014
rudrax14 <m.rudra2003@gmail.com>
2024-06-15 13:20:59 +0530

    28_The Local Search Functionality

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -8,12 +8,24 @@ import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 import Answer from "@/database/answer.model";
 import Interaction from "@/database/interaction.model";
+import { FilterQuery } from "mongoose";
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const questions = await Question.find({})
+    const { searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = {};
+
+    if(searchQuery) {
+      query.$or = [
+        { title: { $regex: new RegExp(searchQuery, "i")}},
+        { content: { $regex: new RegExp(searchQuery, "i")}},
+      ]
+    }
+
+    const questions = await Question.find(query)
       .populate({ path: 'tags', model: Tag })
       .populate({ path: 'author', model: User })
       .sort({ createdAt: -1 })
@@ -43,7 +55,7 @@ export async function createQuestion(params: CreateQuestionParams) {
     // Create the tags or get them if they already exist
     for (const tag of tags) {
       const existingTag = await Tag.findOneAndUpdate(
-        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } }, 
         { $setOnInsert: { name: tag }, $push: { questions: question._id } },
         { upsert: true, new: true }
       )
@@ -52,16 +64,16 @@ export async function createQuestion(params: CreateQuestionParams) {
     }
 
     await Question.findByIdAndUpdate(question._id, {
-      $push: { tags: { $each: tagDocuments } }
+      $push: { tags: { $each: tagDocuments }}
     });
 
     // Create an interaction record for the user's ask_question action
-
+    
     // Increment author's reputation by +5 for creating a question
 
     revalidatePath(path)
   } catch (error) {
-
+    
   }
 }
 
@@ -72,10 +84,10 @@ export async function getQuestionById(params: GetQuestionByIdParams) {
     const { questionId } = params;
 
     const question = await Question.findById(questionId)
-      .populate({ path: 'tags', model: Tag, select: '_id name' })
-      .populate({ path: 'author', model: User, select: '_id clerkId name picture' })
+      .populate({ path: 'tags', model: Tag, select: '_id name'})
+      .populate({ path: 'author', model: User, select: '_id clerkId name picture'})
 
-    return question;
+      return question;
   } catch (error) {
     console.log(error);
     throw error;
@@ -90,20 +102,20 @@ export async function upvoteQuestion(params: QuestionVoteParams) {
 
     let updateQuery = {};
 
-    if (hasupVoted) {
-      updateQuery = { $pull: { upvotes: userId } }
+    if(hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId }}
     } else if (hasdownVoted) {
-      updateQuery = {
+      updateQuery = { 
         $pull: { downvotes: userId },
         $push: { upvotes: userId }
       }
     } else {
-      updateQuery = { $addToSet: { upvotes: userId } }
+      updateQuery = { $addToSet: { upvotes: userId }}
     }
 
     const question = await Question.findByIdAndUpdate(questionId, updateQuery, { new: true });
 
-    if (!question) {
+    if(!question) {
       throw new Error("Question not found");
     }
 
@@ -124,20 +136,20 @@ export async function downvoteQuestion(params: QuestionVoteParams) {
 
     let updateQuery = {};
 
-    if (hasdownVoted) {
-      updateQuery = { $pull: { downvote: userId } }
+    if(hasdownVoted) {
+      updateQuery = { $pull: { downvote: userId }}
     } else if (hasupVoted) {
-      updateQuery = {
+      updateQuery = { 
         $pull: { upvotes: userId },
         $push: { downvotes: userId }
       }
     } else {
-      updateQuery = { $addToSet: { downvotes: userId } }
+      updateQuery = { $addToSet: { downvotes: userId }}
     }
 
     const question = await Question.findByIdAndUpdate(questionId, updateQuery, { new: true });
 
-    if (!question) {
+    if(!question) {
       throw new Error("Question not found");
     }
 
@@ -159,7 +171,7 @@ export async function deleteQuestion(params: DeleteQuestionParams) {
     await Question.deleteOne({ _id: questionId });
     await Answer.deleteMany({ question: questionId });
     await Interaction.deleteMany({ question: questionId });
-    await Tag.updateMany({ questions: questionId }, { $pull: { questions: questionId } });
+    await Tag.updateMany({ questions: questionId }, { $pull: { questions: questionId }});
 
     revalidatePath(path);
   } catch (error) {
@@ -175,7 +187,7 @@ export async function editQuestion(params: EditQuestionParams) {
 
     const question = await Question.findById(questionId).populate("tags");
 
-    if (!question) {
+    if(!question) {
       throw new Error("Question not found");
     }
 
@@ -195,10 +207,10 @@ export async function getHotQuestions() {
     connectToDatabase();
 
     const hotQuestions = await Question.find({})
-      .sort({ views: -1, upvotes: -1 })
+      .sort({ views: -1, upvotes: -1 }) 
       .limit(5);
 
-    return hotQuestions;
+      return hotQuestions;
   } catch (error) {
     console.log(error);
     throw error;
